0ead783bd4776634c43256168c9ffc20cfba4bb5
Saksham27 <geekysaksham27@gmail.com>
2020-03-12 17:20:32 +0530

    Refactor: Divide operation now returns floating point value

--- arithmaticOperations.sh
+++ arithmaticOperations.sh
@@ -3,6 +3,6 @@ read -p "Enter X : " x;
 read -p "Enter Y : " y;
 read -p "Enter Z : " z;
 echo "X+Y*Z = "$(($x+$y*$z));
-echo "Z+X/Y = "$(($z+$x/$y));
+echo "Z+X/Y = "`echo "scale=2; ($z+$x/$y)" | bc`
 echo "X%Y+Z = "$(($x%$y+$z));
 echo "X*Y+Z = "$(($x*$y+$z));
